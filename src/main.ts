6ca09f0757d6103f213ddfd58d77823aa2d83204
husan <abdigafurovhusan@gmail.com>
2023-05-24 18:31:55 +0500

    updat

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { MessagesModule } from './messages/messages.module';
 
 async function bootstrap() {
